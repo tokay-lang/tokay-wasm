7e4005bf4b1a35e9f41e628ad472dba38124a186
Jan Max Meyer <jmm@phorward.de>
2025-05-21 01:48:27 +0200

    feat: First impressions of an UI...

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # tokay-wasm
 
+![My POV on JavaScript...](./homer-js.jpg)
+
 - Build: `wasm-pack build --target=web`
 - Serve: `python -m http.server`
 - Run: [http://localhost:8000/hello.html](http://localhost:8000/hello.html)
--- src/lib.rs
+++ src/lib.rs
@@ -3,16 +3,16 @@ use tokay::{Compiler, Object, Reader, vm::Thread};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn tokay(code: &str) -> Option<String> {
+pub fn tokay(code: &str, input: &str) -> Option<String> {
     let code = code.to_string();
+    let mut input = Reader::new(None, Box::new(io::Cursor::new(input.to_string())));
 
     let mut compiler = Compiler::new();
-    let mut dummy = Reader::new(None, Box::new(io::Cursor::new(String::new())));
 
     match compiler.compile(Reader::new(None, Box::new(io::Cursor::new(code)))) {
         Ok(None) => None,
         Ok(Some(program)) => {
-            let mut thread = Thread::new(&program, vec![&mut dummy]);
+            let mut thread = Thread::new(&program, vec![&mut input]);
             //thread.debug = compiler.debug;
             //thread.globals = globals;
 
